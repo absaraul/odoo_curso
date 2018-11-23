38dafc8a5f101c253efce837ad7e84468211b1b4
Raul Zapata <raul.zapata@grupoabsa.com>
2018-11-23 18:27:56 +0000

    [IMP] openacademy: Adding basic unittest

---
+++ openacademy/test/test_global_openacademy_course.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+
+from psycopg2 import IntegrityError
+from openerp.tests.common import TransactionCase
+from openerp.tools import mute_loggr
+
+class GlobalTestOpenaAcademyCourse(TransactionCase):
+
+    # Method seudo-constructor of test setUp
+    def setUp(self):
+        # Define global variables to test methods
+        super(GlobalTestOpenAcademyCourse,self).setUp()
+        self.variable = 'hello world'
+        self.course = self.env['openacademy.course']
+
+    # Method of class that don't is test
+    def create_course(self, course_name, course_description,
+                      course_responsible_id):
+        course_id = self.course.create({
+            'name': course_name,
+            'description': course_description,
+            'responsible_id': course_responsible_id,
+        })
+        return course_id
+
+    # Method of test startswith 'def test_*(self):'
+
+    # Mute the error openerp.sql_db to don't to avoid it in log
+    @mute_logger('openerp.sql_db')
+    def test_01_same_name_description(self):
+        '''
+        Test create a course with same name and description.
+        To test contraint of name different to description
+        '''
+        with self.assertRaisesRegexp(
+                IntegrityError,,
+                'new row for relation "openacademy_course" violates'
+                ' check constraint "openacademy_course_name_description_check"i'
+                ):
+            # Create a course with same name and description to raise error
+            self.create_course('test'.'test',None)
