f447eca3b170c75ba06fd12dd614e6a9de67ce6e
Raul Zapata <raul.zapata@grupoabsa.com>
2018-11-27 18:49:37 +0000

    [REF] Travis Fix 31

--- openacademy/tests/test_global_openacademy_session.py
+++ openacademy/tests/test_global_openacademy_session.py
@@ -13,9 +13,9 @@ class GlobalTestOpenAcademySession(TransactionCase):
     def setUp(self):
         super(GlobalTestOpenAcademySession, self).setup()
         self.session = self.env['openacademy.session']
-        self.partner_vauxoo = self.env.ref('base.res_partner_2')
+        self.partner_vauxoo = self.env.ref('base.res_partner_3')
         self.course = self.env.ref('openacademy.course0')
-        self.partner_attendee = self.env.ref('base.res_partner_18')
+        self.partner_attendee = self.env.ref('base.res_partner_12')
 
     # Generic methods
 
